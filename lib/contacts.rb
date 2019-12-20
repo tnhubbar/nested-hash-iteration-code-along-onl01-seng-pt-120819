ff84dff7753c7a8172bc2227134cb598c5d901e0
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-20 02:21:59 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,6 +18,6 @@ def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
-        binding.pry 
+    binding.pry 
 end
 end
